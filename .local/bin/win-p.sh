43877b6d40c6f405ca994b1d608da33161e102f6
Bruno <brunoferreira17k@gmail.com>
2026-03-03 00:49:46 -0300

    add all configs for win+p funtionality :)

---
+++ .local/bin/win-p.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Captura a escolha do usuário
+CHOSEN=$(echo -e "Built-in Screen only\nExternal Screen only\nBoth Screens" | fuzzel -d -l 3)
+
+STATUS=$(wlr-randr)
+
+case "$CHOSEN" in
+    "Built-in Screen only")
+        wlr-randr --output eDP-1 --on --pos 0,0
+
+        if echo "$STATUS" | grep -q "^DP-6"; then wlr-randr --output DP-6 --off; fi
+        if echo "$STATUS" | grep -q "^DP-1"; then wlr-randr --output DP-1 --off; fi
+        if echo "$STATUS" | grep -q "^HDMI-A-1"; then wlr-randr --output HDMI-A-1 --off; fi
+        ;;
+
+    "External Screen only")
+      
+        if echo "$STATUS" | grep -q "^DP-6"; then
+            wlr-randr --output DP-6 --on --pos 0,0
+        elif echo "$STATUS" | grep -q "^DP-1"; then
+            wlr-randr --output DP-1 --on --pos 0,0
+        elif echo "$STATUS" | grep -q "^HDMI-A-1"; then
+            wlr-randr --output HDMI-A-1 --on --pos 0,0
+        fi
+
+        wlr-randr --output eDP-1 --off
+        ;;
+
+    "Both Screens")
+        wlr-randr --output eDP-1 --on --pos 0,0
+
+        if echo "$STATUS" | grep -q "^DP-6"; then
+            wlr-randr --output DP-6 --on --pos 1920,0
+        elif echo "$STATUS" | grep -q "^DP-1"; then
+            wlr-randr --output DP-1 --on --pos 1920,0
+        elif echo "$STATUS" | grep -q "^HDMI-A-1"; then
+            wlr-randr --output HDMI-A-1 --on --pos 1920,0
+        fi
+        ;;
+esac
